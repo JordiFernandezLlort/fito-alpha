b923171b4e33695fb7673f202578cb845c1342bf
FishBones2003 <jordiferllort@gmail.com>
2022-10-10 10:23:48 +0200

    Constraints and button declaration

--- app/src/main/java/com/jfernandez/fito_alpha/MainMenu.java
+++ app/src/main/java/com/jfernandez/fito_alpha/MainMenu.java
@@ -3,12 +3,25 @@ package com.jfernandez.fito_alpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
+
+    private ImageButton configuracio, compte, camera, lupa, opcions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        configuracio = findViewById(R.id.botoConfig);
+        compte = findViewById(R.id.botoCompte);
+        camera = findViewById(R.id.botoCamera);
+        lupa = findViewById(R.id.botoLupa);
+        opcions = findViewById(R.id.botoOpcions);
+
+
+
     }
 }
