e33ca26ef020e394fbf740b91730250e6cbd9601
lferrer14 <lferrercoma14@gmail.com>
2022-11-16 10:45:30 +0100

    holahola

--- app/src/main/java/com/jfernandez/fito_alpha/MainMenu.java
+++ app/src/main/java/com/jfernandez/fito_alpha/MainMenu.java
@@ -1,14 +1,33 @@
 package com.jfernandez.fito_alpha;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 
 public class MainMenu extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser user;
     private ImageButton configuracio, compte, camera, lupa, opcions;
 
     @Override
@@ -22,6 +41,10 @@ public class MainMenu extends AppCompatActivity {
         lupa = findViewById(R.id.botoLupa);
         opcions = findViewById(R.id.botoOpcions);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+
         /*configuracio.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View view) {
@@ -30,13 +53,41 @@ public class MainMenu extends AppCompatActivity {
             }
         });*/
 
-        compte.setOnClickListener(new View.OnClickListener(){
+        compte.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), Login.class);
-                startActivity(intent);
+
+                mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                   @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        user = mFirebaseAuth.getCurrentUser();
+                        if (user != null){
+                            Toast.makeText(MainMenu.this, "hi ha usuari (click)", Toast.LENGTH_LONG).show();
+                            //fer daialog
+                            // si esta regsitrat
+                            // AuthUI.getInstance().signOut(this); fara un logout
+                        }else {
+                            Toast.makeText(MainMenu.this, "no hi ha usuari (click)", Toast.LENGTH_LONG).show();
+                            //9. resetegem la informació que contenen els camps informatius del layout. en cas que canviem d'usuari hem de protegir la informac
+                            //11. inici amb google
+                            AuthUI.IdpConfig googleIdp = new AuthUI.IdpConfig.GoogleBuilder().build();
+                            AuthUI.IdpConfig microsoft = new AuthUI.IdpConfig.MicrosoftBuilder().build();
+                            AuthUI.IdpConfig github = new AuthUI.IdpConfig.GitHubBuilder().build();
+                            //1.si no hi ha usuari accedir a firebase i autentificar o donar d'alta
+                            startActivityForResult(AuthUI.getInstance().
+                                    createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setTosUrl("https://joviat.com")
+                                    .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),googleIdp, microsoft,github))
+                                    .build(), RC_SIGN_IN);
+                        }
+                    }
+                };
+                mFirebaseAuth.addAuthStateListener(mAuthStateListener);
             }
         });
+
 
        /* camera.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -62,5 +113,41 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+
     }
+
+
+
+    //2. 14/10/2022
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @NonNull Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Benvingut, Benvinguda...", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Ho sento, ha fallat l'autenticació.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    } //3. 14/10/2022 compilar i donar d'alta un usuari (s'haurà de donar d'alta el servei a firebase console)
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //per activar el servei d'autenticació
+        //mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //deasctivar el servei d'autenticació per no gastar bateria
+        if(mAuthStateListener!=null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
 }
--- app/src/main/java/com/jfernandez/fito_alpha/Registre.java
+++ app/src/main/java/com/jfernandez/fito_alpha/Registre.java
@@ -1,7 +1,9 @@
 package com.jfernandez.fito_alpha;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +17,7 @@ public class Registre extends AppCompatActivity {
     private TextView ptUsuari, ptContrasenya, ptRContrasenya, ptTelefon, ptEmail;
     private Button  btConfirmar;
     private ImageButton btEnrere;
+    public static SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,18 @@ public class Registre extends AppCompatActivity {
         btEnrere = findViewById(R.id.btEnrere);
         btConfirmar = findViewById(R.id.btConfirmar);
 
+        //obtenor l'objecte SheredPreferences
+        prefs = getSharedPreferences("FitxerPreferences", MODE_PRIVATE);
+
+        //posem valors guardats i si no hi ha dades deixem els camps en blanc(valors per defecte)
+        ptUsuari.setText(prefs.getString("Usuari", ""));
+        ptTelefon.setText(prefs.getString("Telefon", ""));
+        ptEmail.setText(prefs.getString("Email", ""));
+        ptContrasenya.setText(prefs.getString("Contrasenya", ""));
+
+
+
+
         btEnrere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,11 +54,32 @@ public class Registre extends AppCompatActivity {
             }
         });
 
+
+
         btConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Registre.this, "Encara no tenim la base de dades feta", Toast.LENGTH_SHORT).show();
+                guardaDades();
+
+                Toast.makeText(Registre.this, "Registre CORRECTE: el teu usuaris és: " + ptUsuari.getText().toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getBaseContext(), MainMenu.class);
+                startActivity(intent);
             }
         });
+    }
+    public void guardaDades(){
+
+        SharedPreferences.Editor editor;
+        editor = prefs.edit();
+
+        editor.putString("Usuari", ptUsuari.getText().toString());
+        editor.putString("CognContrasenyaom", ptContrasenya.getText().toString());
+        editor.putString("Telefon", ptTelefon.getText().toString());
+        editor.putString("Email", ptEmail.getText().toString());
+
+       
+
+
+
     }
 }
